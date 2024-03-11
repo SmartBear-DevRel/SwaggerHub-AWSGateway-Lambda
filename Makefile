6cac871f4341e7869dfb4e11187f736e7e3953cb
Yousaf Nabi <you@saf.dev>
2024-03-11 13:01:16 +0000

    chore: remove localstack for demo

--- Makefile
+++ Makefile
@@ -1,65 +1,22 @@
-.PHONY: docker 
 
-LOCALSTACK_URL:=localhost:4566
 EXPLORE_SPACES_FILE:=ExploreSpaces.json
-# Allow for LocalStack existing running instance
-up:
-	if curl -sSf $$LOCALSTACK_URL> /dev/null; then\
-		make docker_no_localstack;\
-	else\
-		make docker;\
-	fi
-docker:
-	docker-compose up -d --build
-down:
-	docker-compose down
-docker_no_localstack:
-	docker-compose config --services | grep -v localstack | xargs docker-compose up -d 
 
-env_vars:
-	echo $$AWS_ACCESS_KEY_ID
-	echo $$AWS_SECRET_ACCESS_KEY
-	echo $$AWS_SESSION_TOKEN
-s3_copy:
-	awslocal s3 cp --recursive s3://${ARTIFACTS_BUCKET} ./tmp
-s3_create:
-	./docker/init/localstack/buckets_local.sh
-iam_create:
-	./docker/init/localstack/roles_local.sh
-
-logs_localstack:
-	docker logs -f swaggerhub-awsgateway-lambda-localstack-1
-
-sam_pre_reqs: setup_localstack
 sam_test:
 	dotnet test --arch x64 books-api/test/Bookstore.Test/Bookstore.Tests.csproj
 sam_build:
-	samlocal build --template books-api/template.yaml
-sam_package:
-	samlocal package \
-            --s3-bucket $${ARTIFACTS_BUCKET:-swaggerhub-awsgateway-lambda-artifacts} \
-            --region $${AWS_DEFAULT_REGION} \
-            --output-template-file packaged-lambda.yaml
-sam_deploy:
-	samlocal deploy --stack-name $${STACK_NAME:-books-api-dev} \
-            --template packaged-lambda.yaml \
-            --capabilities CAPABILITY_IAM \
-            --region $${AWS_DEFAULT_REGION} \
-            --s3-bucket $${ARTIFACTS_BUCKET:-swaggerhub-awsgateway-lambda-artifacts} \
-            --no-fail-on-empty-changeset \
-            --role-arn $${CLOUDFORMATION_EXECUTION_ROLE_ARN:-arn:aws:iam::000000000000:role/aws-actions-dev-CloudFormationExecutionRole}
-sam_integration_test:
-	dotnet test --arch x64 books-api/test/Bookstore.IntegrationTest/Bookstore.IntegrationTest.csproj
-sam_output_endpoint:
-	@awslocal cloudformation describe-stacks --stack-name books-api-dev --query "Stacks[0].Outputs[0].OutputValue" --output text
-sam_curl_books_endpoint:
-	curl localhost:4566/restapis/$(shell make sam_output_endpoint | cut -d'/' -f3 | awk -F'.' '{print $$1}')/Prod/_user_request_/books | jq .
-sam_curl_books_id_endpoint:
-	curl localhost:4566/restapis/$(shell make sam_output_endpoint | cut -d'/' -f3 | awk -F'.' '{print $$1}')/Prod/_user_request_/books/be05885a-41fc-4820-83fb-5db17015ed4a | jq .
-
-setup_localstack:
-	docker/init/localstack/buckets_local.sh
-	docker/init/localstack/roles_local.sh
+	cd books-api/src/Bookstore && \
+	sam build --template ../../template.yaml --no-cached
+sam_start:
+	cd books-api/src/Bookstore && \
+	sam local start-api --warm-containers eager
+sam_curl_books_endpoint_local:
+	curl http://127.0.0.1:3000/books | jq .
+sam_curl_books_id_endpoint_local:
+	if [[ $${BOOK_ID} == "" ]]; then \
+		echo BOOK_ID must be set;\
+		exit 1;\
+	fi;
+	curl http://127.0.0.1:3000/books/$${BOOK_ID} | jq .
 
 install_explore_cli:
 	dotnet tool install --global Explore.Cli
--- runWin.ps1
+++ runWin.ps1
@@ -1,111 +1,28 @@
-$LOCALSTACK_URL = "localhost:4566"
 
-# Allow for LocalStack existing running instance
-function docker_up {
-	if (Test-Connection -ComputerName $LOCALSTACK_URL -Quiet) {
-		docker_no_localstack
-	} else {
-		docker_up_build
-	}
-}
-
-function docker_up_build {
-	docker-compose up -d --build
-}
-
-function docker_down {
-	docker-compose down
-}
-
-function docker_no_localstack {
-	docker-compose config --services | Select-String -Pattern "localstack" -NotMatch | ForEach-Object { docker-compose up -d $_ }
-}
-
-function print_env_vars {
-	Write-Host $env:AWS_ACCESS_KEY_ID
-	Write-Host $env:AWS_SECRET_ACCESS_KEY
-	Write-Host $env:AWS_SESSION_TOKEN
-}
-
-function s3_copy {
-	$artifactsBucket = if ($env:ARTIFACTS_BUCKET) { $env:ARTIFACTS_BUCKET } else { "swaggerhub-awsgateway-lambda-artifacts" };
-	awslocal s3 cp --recursive s3://$artifactsBucket ./tmp
-}
-
-function s3_create {
-	./docker/init/localstack/buckets_local.sh
-}
-
-function iam_create {
-	./docker/init/localstack/roles_local.sh
-}
-
-function logs_localstack {
-	docker logs -f swaggerhub-awsgateway-lambda-localstack-1
-}
-
-function sam_pre_reqs {
-	setup_localstack
-}
 
 function sam_test {
 	dotnet test --arch x64 books-api/test/Bookstore.Test/Bookstore.Tests.csproj
 }
 
 function sam_build {
-	sam build --template books-api/template.yaml
+	cd books-api/src/Bookstore; sam build --template ../../template.yaml --no-cached
+}
+function sam_start {
+	cd books-api/src/Bookstore; sam local start-api --warm-containers eager
 }
 
-function sam_package {
-	$artifactsBucket = if ($env:ARTIFACTS_BUCKET) { $env:ARTIFACTS_BUCKET } else { "swaggerhub-awsgateway-lambda-artifacts" };
-	$region = if ($env:AWS_DEFAULT_REGION) { $env:AWS_DEFAULT_REGION } else { "us-east-1" };
-	samlocal package `
-		--s3-bucket $artifactsBucket `
-		--region $region `
-		--output-template-file packaged-lambda.yaml `
-		--profile localstack
-}
-function sam_deploy {
-	$artifactsBucket = if ($env:ARTIFACTS_BUCKET) { $env:ARTIFACTS_BUCKET } else { "swaggerhub-awsgateway-lambda-artifacts" };
-	$stackName = if ($env:STACK_NAME) { $env:STACK_NAME } else { "books-api-dev" };
-	$region = if ($env:AWS_DEFAULT_REGION) { $env:AWS_DEFAULT_REGION } else { "us-east-1" };
-	$cloudformationRoleArn = if ($env:CLOUDFORMATION_EXECUTION_ROLE_ARN) { $env:CLOUDFORMATION_EXECUTION_ROLE_ARN } else { "arn:aws:iam::000000000000:role/aws-actions-dev-CloudFormationExecutionRole" };
-
-	samlocal deploy `
-		--stack-name $stackName `
-		--template packaged-lambda.yaml `
-		--capabilities CAPABILITY_IAM `
-		--region $region `
-		--s3-bucket  $artifactsBucket `
-		--no-fail-on-empty-changeset `
-		--role-arn $cloudformationRoleArn`
-		--profile localstack
-}
-
-function sam_integration_test {
-	dotnet test --arch x64 books-api/test/Bookstore.IntegrationTest/Bookstore.IntegrationTest.csproj
-}
-
-function sam_output_endpoint {
-	$region = if ($env:AWS_DEFAULT_REGION) { $env:AWS_DEFAULT_REGION } else { "us-east-1" };
-	aws cloudformation describe-stacks --stack-name books-api-dev --query "Stacks[0].Outputs[0].OutputValue" --output text --region $region --endpoint http://localhost:4566 --profile localstack
-}
-
-function sam_curl_books_endpoint {
-	$apiId = (sam_output_endpoint).Split('//')[1].Split('.')[0]
-	$url = "http://localhost:4566/restapis/$apiId/Prod/_user_request_/books"
+function sam_curl_books_endpoint_local {
+	$url = "http://127.0.0.1:3000/books"
 	Invoke-RestMethod -Uri $url | ConvertTo-Json
 }
 
-function sam_curl_books_id_endpoint {
-	$apiId = (sam_output_endpoint).Split('//')[1].Split('.')[0]
-	$url = "http://localhost:4566/restapis/$apiId/Prod/_user_request_/books/be05885a-41fc-4820-83fb-5db17015ed4a"
+function sam_curl_books_id_endpoint_local {
+	if ($env:BOOK_ID -eq "") {
+		Write-Host "please set BOOK_ID env var"
+		exit 1
+	}
+	$url = "http://127.0.0.1:3000/books/$env:BOOK_ID"
 	Invoke-RestMethod -Uri $url | ConvertTo-Json
-}
-
-function setup_localstack {
-	& ./docker/init/localstack/buckets_local.sh
-	& ./docker/init/localstack/roles_local.sh
 }
 
 function install_explore_cli {
