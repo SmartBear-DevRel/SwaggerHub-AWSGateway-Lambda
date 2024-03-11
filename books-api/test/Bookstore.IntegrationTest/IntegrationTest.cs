c11468edc7be3b0aeca6479031f51503eabe08a9
Frank Kilcommins <frank.kilcommins@smartbear.com>
2024-03-11 10:13:18 +0000

    update stack and integration test

--- books-api/test/Bookstore.IntegrationTest/IntegrationTest.cs
+++ books-api/test/Bookstore.IntegrationTest/IntegrationTest.cs
@@ -12,7 +12,7 @@ public class IntegrationTests : IClassFixture<Setup>
     [Fact]
     public async void GetBooks_ShouldReturnOk()
     {
-
+        _httpClient.DefaultRequestHeaders.Add("X-Api-Key", "91e69fe1-fefd-4b2d-b689-032fb0947d10");
         var getBooksResponse = await this._httpClient.GetAsync($"{Setup.ApiUrl}/books");
 
         Assert.Equal(HttpStatusCode.OK, getBooksResponse.StatusCode);
